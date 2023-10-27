ad6403bd9a1856c9fa7dde29978387942d074abc
Thomipon <38535540+Thomipon@users.noreply.github.com>
2023-10-27 14:55:30 +0200

    Add flip flop thingy

---
+++ Source/EntropyNodes/Private/EntropyNodes_FlipFlopHandle.cpp
@@ -0,0 +1,21 @@
+﻿#include "EntropyNodes_FlipFlopHandle.h"
+
+FFlipFlopHandle::FFlipFlopHandle(const TFunction<void()>& Flip, const TFunction<void()>& Flop)
+	: Flip(Flip),
+	  Flop(Flop)
+{
+}
+
+FFlipFlopHandle& FFlipFlopHandle::Execute()
+{
+	if (bFlip)
+	{
+		Flip();
+	}
+	else
+	{
+		Flop();
+	}
+	bFlip = !bFlip;
+	return *this;
+}
---
+++ Source/EntropyNodes/Public/EntropyNodes_FlipFlopHandle.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+/**
+ * Handle for a FlipFlop.<br>
+ * Handles need to be kept alive to keep their state!
+ */
+struct FFlipFlopHandle
+{
+private:
+	TFunction<void()> Flip;
+	TFunction<void()> Flop;
+	bool bFlip = true;
+
+public:
+	/**
+	 * @param Flip First function to be executed
+	 * @param Flop Second function to be executed
+	 */
+	FFlipFlopHandle(const TFunction<void()>& Flip, const TFunction<void()>& Flop);
+
+	/**
+	 * Executes one of the functions associated with this handle depending on its state
+	 * @return A reference to the handle it was executed on to support builder pattern like stuff
+	 */
+	FFlipFlopHandle& Execute();
+};
