c2abaf415b7cbc3f55c6013f2e337adb464aac4c
Thomipon <38535540+Thomipon@users.noreply.github.com>
2023-10-27 13:44:14 +0200

    Initial commit

---
+++ Source/EntropyNodes/Private/DoOnceHandle.cpp
@@ -0,0 +1,23 @@
+﻿#include "DoOnceHandle.h"
+
+FDoOnceHandle::FDoOnceHandle(const TFunction<void()>& Function, bool bStartClosed)
+	: Function(Function),
+	  bOpen(!bStartClosed)
+{
+}
+
+FDoOnceHandle& FDoOnceHandle::Do()
+{
+	if (bOpen)
+	{
+		bOpen = false;
+		Function();
+	}
+	return *this;
+}
+
+FDoOnceHandle& FDoOnceHandle::Reset()
+{
+	bOpen = true;
+	return *this;
+}
---
+++ Source/EntropyNodes/Public/DoOnceHandle.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+/**
+ * Handle for a DoOnce.<br>
+ * Handles need to be kept alive to keep their state!<br>
+ */
+struct FDoOnceHandle
+{
+private:
+	const TFunction<void()> Function;
+	bool bOpen;
+
+public:
+	/**
+	 * @param Function The function to execute by Do()
+	 * @param bStartClosed Whether we start closed
+	 */
+	explicit FDoOnceHandle(const TFunction<void()>& Function, bool bStartClosed = false);
+
+	/**
+	 * Execute the assigned function if we are open
+	 * @return The handle it was executed on (returned for support for builder pattern like stuff, i.e., FDoOnceHandle([]{}).Do())
+	 */
+	FDoOnceHandle& Do();
+
+	/**
+	 * Resets the Handle to open
+	 * @return The handle it was executed on (returned for support for builder pattern like stuff)
+	 */
+	FDoOnceHandle& Reset();
+};
